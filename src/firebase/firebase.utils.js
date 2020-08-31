955ac3468f0b0064eb15eec2908f38cb7b846cb6
Marco <marcoragusa77@gmail.com>
2020-08-31 16:25:01 +0200

    incluso firebase utils e aggiunto lo storage dei dati nel firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,33 @@ const config = {
     appId: "1:200128508622:web:47f2f6647ba386f8b4969f"
   };
 
+  export const createUserProfileDocument = async (userAuth, additionalData) =>{
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists){
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (error) {
+        console.log("error creating user", error.message);
+      }
+    }
+    return userRef;
+  }
+
+
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
